e00582155e6dbed77b9fe94ddad59c501467bc18
Ajay Venkatesh Gunturu <ajay@riktamtech.com>
2024-09-21 14:59:19 +0530

    add font to the project

---
+++ src/styles/font.ts
@@ -0,0 +1,42 @@
+import {TextStyle} from 'react-native';
+
+// font weight: 900
+const black: TextStyle = {
+  fontFamily: 'Poppins-Black',
+};
+
+// font weight: 700
+const bold: TextStyle = {
+  fontFamily: 'Poppins-Bold',
+};
+
+// font weight: 600
+const semiBold: TextStyle = {
+  fontFamily: 'Poppins-SemiBold',
+};
+
+// font weight: 500
+const medium: TextStyle = {
+  fontFamily: 'Poppins-Medium',
+};
+
+// font weight: 400
+const regular: TextStyle = {
+  fontFamily: 'Poppins-Regular',
+};
+
+// font weight: 300
+const light: TextStyle = {
+  fontFamily: 'Poppins-Light',
+};
+
+const font = {
+  black,
+  bold,
+  semiBold,
+  medium,
+  regular,
+  light,
+};
+
+export default font;
